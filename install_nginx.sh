00474044427d30deebf0ef954cbbe1369ca9ab01
Abu-Obaida-7 <malikobaida75@gmail.com>
2026-07-22 00:40:17 +0500

    Remove all files from tracking

--- install_nginx.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo apt-get update
-sudo apt-get install nginx -y
-sudo systemctl start nginx
-sudo systemctl enable nginx
-sudo systemctl status nginx
-
-echo "<h1>Welcome to Nginx Web Server</h1>" | sudo tee /var/www/html/index.html
